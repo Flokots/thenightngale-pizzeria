0035dbc83092b5eae1dd8d283a441e3f9d4d5eb4
FloKots <florencekotohoyoh@gmail.com>
2022-03-22 11:32:03 +0300

    fix grandtotal function bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
+//Business Logic
+
   $("table").on('input', '.txtCal', function () {
     // code logic here
-     var getValue=$(this).val();
-     console.log(getValue);
- });
+    var getValue = $(this).val();
+    console.log(getValue);
+  });
   $("#order-form").submit(function (event) {
-   
+
     //Get values from the forms
     var pizzaSize = $("#size option:selected").val();
     var pizzaCrust = $("#crust option:selected").val();
@@ -26,9 +28,9 @@ $(document).ready(function () {
     //Calculate Total Cost
     var total = parseInt(pizzaSize) + topingsCost + parseInt(pizzaCrust);
     console.log("Total: " + total);
-    
 
-    //Add Text to Table Row
+
+    //User Interface Logic
     $("#table-size").html($("#table-size option:selected").text() + pizzaSize);
     $("#table-toppings").html($("#toppings option:selected").text() + topingsCost);
     $("#table-crust").html($("#table-crust option:selected").text() + pizzaCrust);
@@ -40,6 +42,7 @@ $(document).ready(function () {
     this.reset();
   })
 })
+
 
 
 function submitForm() {
@@ -75,15 +78,15 @@ function submitForm() {
 
   var newPizza = new Pizza(pizzaSize, topingsCost, pizzaCrust, total, order);
   console.log(newPizza);
-  var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="table-size">' + $("#table-size option:selected").text() + newPizza.size + '</td><td id="table-toppings">' + $("#table-toppings option:selected").text() + newPizza.toppings + '</td><td id="table-crust">' + $("#table-crust option:selected").text() + newPizza.crust + '</td><td id="total" class="total">' + newPizza.total + '</td></tr>'
+  var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="table-size">' + $("#table-size option:selected").text() + newPizza.size + '</td><td id="table-toppings">' + $("#table-toppings option:selected").text() + newPizza.toppings + '</td><td id="table-crust">' + $("#table-crust option:selected").text() + newPizza.crust + '</td><td class="newtotal">' + newPizza.total + '</td></tr>'
 
-  
+
   $("#pizza").append(newRow);
 
   document.getElementById("order-form").reset();
 }
 
-
+//Business Logic
 
 var deliveryCharge = 0;
 function checkout() {
@@ -91,24 +94,32 @@ function checkout() {
   let delivery = confirm("Would you like us to deliver your order?");
   if (delivery) {
     let location = prompt("Please Enter Your Location");
-    alert(`Your order will be delivered to ${location}`)
-    
+    alert(`Your order will be delivered to ${location}. Delivery cost is Ksh. 200`)
     return deliveryCharge = 200;
+  } else {
+    return deliveryCharge = 0;
   }
-
-  
+}
+var subTotalOne = 0;
+function subTotal1() {
+  $('.total').each(function () {
+    subTotalOne = subTotalOne + parseFloat($("#total").text());
+    return subTotalOne;
+  });
 }
 
+var subTotalTwo = 0;
+function subTotal2() {
+  $('.newtotal').each(function () {
+    subTotalTwo = subTotalTwo + parseFloat($(".newtotal").text());
+    return subTotal2;
+  })
+}
 function calculateGrandTotal() {
-  $('#total').each(function() {
-      var grandTotal = 0;
-      grandTotal += parseFloat($(this).text());
-      checkout();
-      grandTotal = grandTotal + deliveryCharge;
-      $('#grandtotal').text(grandTotal);
-             console.log("grandTotal: " + grandTotal);
-  });  
+  subTotal1();
+  subTotal2();
+  checkout();
+  var grandTotal = parseFloat(subTotalOne) + parseFloat(subTotalTwo) + parseFloat(deliveryCharge);
+  $('#grandtotal').text(grandTotal);
+  console.log("grandTotal: " + grandTotal);
 }
-
-
-
